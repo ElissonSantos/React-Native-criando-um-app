6230570f7078885bd65c33f8d126b69e021a2afa
elissonsantos <elissonmaycon@gmail.com>
2023-03-18 22:28:18 -0300

    Utilizando fonts do google

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import React from "react";
-import { SafeAreaView, StatusBar } from "react-native";
+import { SafeAreaView, StatusBar, View } from "react-native";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
 
 import Cesta from "./src/telas/Cestas";
 
 export default function App() {
+  const [fontCarregada] = useFonts({
+    MontserratRegular: Montserrat_400Regular,
+    MontserratBold: Montserrat_700Bold,
+  });
+
+  if (!fontCarregada) {
+    return <View />;
+  }
+
   return (
     <SafeAreaView style={{ marginTop: 50 }}>
       <StatusBar />
