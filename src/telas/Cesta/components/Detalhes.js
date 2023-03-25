e64b9b40d89253625bc492b02d94d79bc86624d0
elissonsantos <elissonmaycon@gmail.com>
2023-03-24 22:37:51 -0300

    Botao de compra

--- src/telas/Cesta/components/Detalhes.js
+++ src/telas/Cesta/components/Detalhes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 import Texto from "../../../components/Texto";
 
@@ -9,6 +9,7 @@ export default function Detalhes({
   nomeFazenda,
   descricao,
   preco,
+  botao,
 }) {
   return (
     <>
@@ -21,6 +22,10 @@ export default function Detalhes({
 
       <Texto style={estilos.descricao}>{descricao}</Texto>
       <Texto style={estilos.preco}>{preco}</Texto>
+
+      <TouchableOpacity style={estilos.botao}>
+        <Texto style={estilos.textoBotao}>{botao}</Texto>
+      </TouchableOpacity>
     </>
   );
 }
@@ -56,5 +61,18 @@ const estilos = StyleSheet.create({
     fontSize: 26,
     lineHeight: 42,
     marginTop: 8,
+  },
+  botao: {
+    marginTop: 16,
+    backgroundColor: "#2A9F85",
+    paddingVertical: 16,
+    borderRadius: 6,
+  },
+  textoBotao: {
+    textAlign: "center",
+    color: "#FFFFFF",
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: "bold",
   },
 });
